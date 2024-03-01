f46a02484c39e94afd47999341e5552507711d69
Garoze <marcelogaroze@hotmail.com>
2024-03-01 09:22:00 -0300

    feat: kinda of a hello world using discord js and typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello, World!');
+import 'dotenv/config';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.MessageContent,
+	],
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.BOT_TOKEN);
