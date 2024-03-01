673aa7d685c4c4f97e2f8b256ac24ae89a892c12
Garoze <marcelogaroze@hotmail.com>
2024-03-01 12:29:13 -0300

    feat: command and event handlers working

---
+++ src/events/Ready.event.ts
@@ -0,0 +1,12 @@
+import { Client, Events } from 'discord.js';
+
+const data = {
+	name: Events.ClientReady,
+	once: true,
+};
+
+const execute = (client: Client) => {
+	console.log(`Ready! Logged in as ${client.user?.tag}`);
+};
+
+export default { data, execute };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import { CommandHandler } from './commands/CommandHandler';
+import { Client, GatewayIntentBits } from 'discord.js';
+import { CommandHandler } from './commands/CommandsHandler';
+import { EventHandler } from './events/EventsHandler';
 
 const client = new Client({
 	intents: [
@@ -12,33 +13,6 @@ const client = new Client({
 });
 
 client.commands = new CommandHandler().loadCommands();
-
-client.once(Events.ClientReady, (readyClient) => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
-
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const cmd = interaction.client.commands.get(interaction.commandName);
-
-	if (!cmd) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await cmd.execute(interaction);
-	}
-	catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-		else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
+client.events = new EventHandler(client).loadEvents();
 
 client.login(process.env.BOT_TOKEN);
